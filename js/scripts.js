722e27e8dc5e0f19f4264b54feeb3acb88f81c58
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2023-10-02 19:13:40 -0500

    I deleted & added arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-alert('Hello world')
-let favoriteFood = "chciken";
-document.write(favoriteFood);
+let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: 'grass' , 'Monster'
+    },
+    {
+      name: 'Ekans',
+      height: 2,
+      types: 'Dragon', 'Field'
+    },
+    {
+      name: 'Arbok',
+      height: 3.5,
+      types: 'Field' , 'Dragon'
+    }
+  ];
