d29e0c453e1b23e5fa9bfd344572b177ed8eaef8
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2023-10-03 04:08:49 -0500

    Types needed to be an array of strings

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonList = [
-    {
-      name: 'Bulbasaur',
-      height: 0.7,
-      types: 'grass' , 'Monster'
-    },
-    {
-      name: 'Ekans',
-      height: 2,
-      types: 'Dragon', 'Field'
-    },
-    {
-      name: 'Arbok',
-      height: 3.5,
-      types: 'Field' , 'Dragon'
-    }
-  ];
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['Grass', 'Monster']
+  },
+  {
+    name: 'Ekans',
+    height: 2,
+    types: ['Dragon', 'Field']
+  },
+  {
+    name: 'Arbok',
+    height: 3.5,
+    types: ['Field', 'Dragon']
+  }
+];
