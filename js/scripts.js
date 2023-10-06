3496e2454b508c175bcdd47c779843d1a94fab19
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2023-10-06 17:45:59 -0500

    added document.write

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@ let pokemonList = [
   },
 ];
 
-for (let i = 0; i < pokemonList.length; i++) 
+for (let i = 0; i < pokemonList.length; i++) {
   document.write(
     pokemonList[i].name + " (height:" + pokemonList[i].height + ")"
   );
@@ -24,10 +24,11 @@ for (let i = 0; i < pokemonList.length; i++)
   // write the if statement to only check for pokemon that have a height above 3
   if (pokemonList[i].height > 3 && pokemonList[i].height < 0.5) {
     document.write(" - wow that's big!");
-  } 
-  else if (pokemonList[i].height > 1.5 && pokemonList[i] < 1) {
-    document.write('- that is ekans')
   }
-  else (pokemonList[i].height > 0.5 && pokemonList[i] < 0.1); {
-    document.write('it is a Bulbasaur')
-  }
+  // use document.write to write two line breaks. You can add the line breaks together with the + operator like you've done above
+  // A line break is written with "<br>"" and will allow each pokemon to be on a new line.
+
+  document.write("<br>");
+  document.write("<br>");
+
+}
