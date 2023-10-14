ec311e35bc40bbeaf67bcffa33d217fe13f24e4f
Mixtapejacob <144938167+Mixtapejacob@users.noreply.github.com>
2023-10-14 08:58:25 -0500

    Code updated for task 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,76 @@
-let pokemonList = [
-  {
-    name: "Bulbasaur",
-    height: 0.7,
-    types: ["Grass", "Monster"],
-  },
-  {
-    name: "Ekans",
-    height: 2,
-    types: ["Dragon", "Field"],
-  },
-  {
-    name: "Arbok",
-    height: 3.5,
-    types: ["Field", "Dragon"],
-  },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["Grass", "Monster"],
+    },
+    {
+      name: "Ekans",
+      height: 2,
+      types: ["Dragon", "Field"],
+    },
+    {
+      name: "Arbok",
+      height: 3.5,
+      types: ["Field", "Dragon"],
+    },
+  ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(
-    pokemonList[i].name + " (height:" + pokemonList[i].height + ")"
-  );
+  for (let i = 0; i < pokemonList.length; i++) {
+    document.write(
+      pokemonList[i].name + " (height:" + pokemonList[i].height + ")"
+    );
 
-  // write the if statement to only check for pokemon that have a height above 3
-  if (pokemonList[i].height > 3 ){
-    document.write(" - wow that's big!");
+    // write the if statement to only check for pokemon that have a height above 3
+    if (pokemonList[i].height > 3) {
+      document.write(" - wow that's big!");
+    }
+    // use document.write to write two line breaks. You can add the line breaks together with the + operator like you've done above
+    // A line break is written with "<br>"" and will allow each pokemon to be on a new line.
+
+    document.write("<br>" + "<br>");
   }
-  // use document.write to write two line breaks. You can add the line breaks together with the + operator like you've done above
-  // A line break is written with "<br>"" and will allow each pokemon to be on a new line.
+  pokemonList.forEach(function (pokemon) {
+    console.log(pokemon.name + "inside the function");
+  });
 
-  document.write("<br>" + "<br>");
-  
 
-}
+
+
+  return {
+    add: function (pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function () {
+      return pokemonList;
+    }
+  };
+
+
+  // console.log(pokemonRepository1.getAll()); // []
+  // pokemonRepository1.add({ name: 'Pikachu' });
+  // console.log(pokemonRepository1.getAll()); // [ { name: 'Pikachu' } ]
+
+  // let pokemonRepository = (function () {
+  //   let pokemonList = [];
+
+  //   function add(pokemon) {
+  //     pokemonList.push(pokemon);
+  //   }
+
+  //   function getAll() {
+  //     return pokemonList;
+  //   }
+
+  //   return {
+  //     add: add,
+  //     getAll: getAll
+  //   };
+
+})();
+
+console.log(pokemonRepository.getAll()); // []
+pokemonRepository.getAll().forEach(function (pokemon) {
+  console.log(pokemon.name)
+});
